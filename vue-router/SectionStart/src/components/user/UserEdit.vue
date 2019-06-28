b6c3cf8c403da753fe755e312eeb6cd6ccb193b6
Eleazar Gaitan <elotgamu@gmail.com>
2019-06-28 07:45:46 -0600

    Updated Vue-Router

---
+++ vue-router/SectionStart/src/components/user/UserEdit.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>Edit the User</h3>
+
+    <p>Query params provided:</p>
+    <p>Locale: {{ $route.query.locale }}</p>
+    <p>Q: {{ $route.query.q }}</p>
+  </div>
+</template>
---
+++ vue-router/SectionStart/src/router.js
@@ -0,0 +1,25 @@
+import Home from "./components/Home.vue";
+import User from "./components/user/User.vue";
+import UserStart from "./components/user/UserStart.vue";
+import UserDetail from "./components/user/UserDetail.vue";
+import UserEdit from "./components/user/UserEdit.vue";
+
+const routes = [
+  {
+    path: "",
+    component: Home,
+    name: "home"
+  },
+  {
+    path: "/user",
+    component: User,
+    children: [
+      { path: "", component: UserStart },
+      { path: ":id", component: UserDetail },
+      { path: ":id/edit", component: UserEdit, name: "userEdit" }
+    ]
+  },
+  { path: "*", redirect: { name: "home" } }
+];
+
+export default routes;
